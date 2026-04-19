d3151e27c29231791dba0ad65eb1b945be2afb12
jgomezo19-spec <jgomezo19@miumg.edu.gt>
2026-04-18 20:46:59 -0600

    primer commit

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author ezkab
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+         Sign_in SIG = new Sign_in();
+         SIG.setVisible(true);
+        
+    }
+}
